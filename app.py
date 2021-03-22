56beb3fce1c185942a6a761d8a4d1cd76cdf8add
Sujit S <44827942+sudevansujit@users.noreply.github.com>
2021-03-22 23:44:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+
+app = Flask(__name__)
+model = pickle.load(open('nlp.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    text_features =  [x for x in request.form.values() ]
+    final_features = ''.join(text_features)
+    prediction = model(final_features).cats['DOCUMENTATION']
+
+    output = round(prediction, 4)
+
+    return render_template('index.html', prediction_text='Documntation Prediction Score is {}'.format(output))
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
